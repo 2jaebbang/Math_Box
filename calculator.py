b7ff0762dc5e5bad27312664e744e0125cdd2ed3
jaebbang <2jaebbang@naver.com>
2021-03-13 21:28:08 +0900

    Revert "test"
    This reverts commit 1e9c23c946a463c14fe4a28f96ff0ea220a76738.

--- calculator.py
+++ calculator.py
@@ -8,9 +8,17 @@ def subtract(a,b):
 def multiply(a,b):
 	return a*b
 
+<<<<<<< HEAD
 def divide_free(a,b):
 	return a/b
 
 def square(a):
 	return a*a	
+=======
+	def divide_free(a,b):
+	return a/b
 
+def sayhello(){
+	print("hello")
+}
+>>>>>>> 7f8f56cb9eaec18a7f7045a1deacf55e6d9de546
